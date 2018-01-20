2f7b8a4482220912f2a64dc0e40b9fac9a10b293
Uzma Khan <u_kam_khan@hotmail.com>
2018-01-20 16:02:14 -0500

    adding blank spring boot stuff

---
+++ src/main/java/com/android/dictionary/dictrank/DictRankApplication.java
@@ -0,0 +1,12 @@
+package com.android.dictionary.dictrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictRankApplication.class, args);
+	}
+}
